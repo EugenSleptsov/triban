a67923d5220b6ec619743bbfb0d5aa2468312a78
Evgeniy Sleptsov <slepcov@unitiki.com>
2024-06-06 16:15:32 +0300

    .env support

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"github.com/EugenSleptsov/triban/bot"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
+	// Load environment variables from the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get the TELEGRAM_TOKEN from the environment
+	telegramToken := os.Getenv("TELEGRAM_TOKEN")
+	if telegramToken == "" {
+		log.Fatalf("TELEGRAM_TOKEN is not set in the environment")
+	}
+
 	botAPI, err := bot.NewBotAPI("")
 	if err != nil {
 		log.Panic(err)
